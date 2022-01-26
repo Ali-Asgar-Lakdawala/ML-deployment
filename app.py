1432856027014ac09a4544c9a20257c71a7bd08d
Ali Asgar Lakdawala <87444913+Ali-Asgar-Lakdawala@users.noreply.github.com>
2022-01-27 00:42:29 +0530

    v3.4

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def main ():
     if choice == "Bike sharing demand prediction":    
         st.header('Bike sharing demand prediction')                                   
 
-        date = st.date_input("Date for demand prediction",datetime.date(2021, 1, 1))
+        date = st.date_input("Date for demand prediction",datetime.date(2021, 3, 6))
         month=date.month
         weekday=date.weekday()
 
@@ -36,13 +36,13 @@ def main ():
             weekend = 0
 
         Hour = st.slider("Hour",value =15,max_value=23,min_value=0)
-        Holiday=st.checkbox('Holiday', value=False)
+        Holiday=st.checkbox('Holiday', value=True)
         if Holiday==0:
             Holiday_No_Holiday=0
         else :
             Holiday_No_Holiday=1
 
-        Functioning_Day=st.checkbox('Functioning_Day', value=False)
+        Functioning_Day=st.checkbox('Functioning_Day', value=True)
         if Functioning_Day==0:
             Functioning_Day_Yes=0
         else :
@@ -69,7 +69,7 @@ def main ():
         Humidity_per = st.number_input("Humidity_per",value =14)
         Wind_speed_m_per_sec = st.number_input("Wind_speed_m_per_sec",value =2.2)
         Visibility_10m = st.number_input("Visibility_10m",value =1828)
-        Dew_point_temp_c = st.number_input("Dew_point_temp_c",value =15)
+        Dew_point_temp_c = st.number_input("Dew_point_temp_c",value =-15)
         Solar_Radiation = st.number_input("Solar_Radiation",value =2.33)
         Rainfall_mm = st.number_input("Rainfall_mm",value =0)
         Snowfall_cm = st.number_input("Snowfall_cm",value =0)
