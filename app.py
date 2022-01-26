1755350fb9e1019b5484b6eac2eea9437daa51c1
Ali Asgar Lakdawala <87444913+Ali-Asgar-Lakdawala@users.noreply.github.com>
2022-01-26 22:32:43 +0530

    3.2

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-#import numpy as np
 import pickle
-#import pandas as pd
 import streamlit as st 
+import datetime
 
 pickle_in= open('bike_demand_pred_model.pkl','rb')
 regressor=pickle.load(pickle_in)
@@ -26,6 +25,29 @@ def main ():
 
     if choice == "Bike sharing demand prediction":    
         st.header('Bike sharing demand prediction')                                   
+
+        date = st.date_input("Date for demand prediction",datetime.date(2021, 1, 1))
+        month=date.month
+        weekday=date.weekday()
+
+        if weekday > 4:
+            weekend = 1
+        else :
+            weekend = 0
+
+        Hour = st.slider("Hour",value =15,max_value=23,min_value=0)
+        Holiday=st.checkbox('Holiday', value=False)
+        if Holiday==0:
+            Holiday_No_Holiday=0
+        else :
+            Holiday_No_Holiday=1
+
+        Functioning_Day=st.checkbox('Functioning_Day', value=False)
+        if Functioning_Day==0:
+            Functioning_Day_Yes=0
+        else :
+            Functioning_Day_Yes=1
+
         Seasons = st.selectbox("Seasons",['Spring','Summer','Winter','Autumn'])
         if Seasons=='Spring':
             Seasons_Spring = 1
@@ -43,19 +65,6 @@ def main ():
             Seasons_Spring = 0
             Seasons_Summer = 0
             Seasons_Winter = 1
-
-        Holiday=st.checkbox('Holiday', value=False)
-        if Holiday==0:
-            Holiday_No_Holiday=0
-        else :
-            Holiday_No_Holiday=1
-
-        Functioning_Day=st.checkbox('Functioning_Day', value=False)
-        if Functioning_Day==0:
-            Functioning_Day_Yes=0
-        else :
-            Functioning_Day_Yes=1
-        Hour = st.number_input("Hour",value =15,max_value=23,min_value=1)
         Temperature_C = st.number_input("Temperature_C",value =16)
         Humidity_per = st.number_input("Humidity_per",value =14)
         Wind_speed_m_per_sec = st.number_input("Wind_speed_m_per_sec",value =2.2)
@@ -64,16 +73,10 @@ def main ():
         Solar_Radiation = st.number_input("Solar_Radiation",value =2.33)
         Rainfall_mm = st.number_input("Rainfall_mm",value =0)
         Snowfall_cm = st.number_input("Snowfall_cm",value =0)
-        month = st.number_input("month",value =3,max_value=12,min_value=1)
-        weekend = st.selectbox("weekend",['yes','no'])
-        if weekend=='yes':
-            weekdays_weekend = 1
-        else :
-            weekdays_weekend = 0
 
         if st.button("Predict count"):
             result= regressor.predict([[Seasons_Spring,Seasons_Summer,Seasons_Winter,Holiday_No_Holiday,Functioning_Day_Yes,Hour,Temperature_C,Humidity_per
-                                    ,Wind_speed_m_per_sec,Visibility_10m,Dew_point_temp_c,Solar_Radiation,Rainfall_mm,Snowfall_cm,month,weekdays_weekend]])    
+                                    ,Wind_speed_m_per_sec,Visibility_10m,Dew_point_temp_c,Solar_Radiation,Rainfall_mm,Snowfall_cm,month,weekend]])    
             st.success('The output is {}'.format(result))
 
     elif choice == "Contack Us":
